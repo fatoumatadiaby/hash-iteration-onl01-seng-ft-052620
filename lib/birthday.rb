e69aafc99d3b857444a4c8db97721ea966e84fb3
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-03 15:37:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,6 @@ puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
+  if  birthday_kids.each do |kids_name, age|
    "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end 
